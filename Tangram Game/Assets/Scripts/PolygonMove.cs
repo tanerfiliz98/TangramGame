43456b315f9cfeca33792420294e6a755da4a4d9
tanerfiliz98 <tanerfiliz98@gmail.com>
2021-05-31 15:08:36 +0300

    Create Move Scripts
    Polygon and Quadrilateral move scripts added

--- Tangram Game/Assets/Scripts/PolygonMove.cs
+++ Tangram Game/Assets/Scripts/PolygonMove.cs
@@ -1,18 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PolygonMove : MonoBehaviour
 {
+    const float doubleClickTime = 0.2f;
+    float lastClickTime = 0.0f;
+    bool pieceStatusLock = false;
+    bool mouseMoving;
+    Collider2D lockObj;
+    public Text puan;
+    public int TotalPieceNumber;
+    public string NextLevelName;
+
+    private void collisonCheck(Collider2D col)
+    {
+        if (col.gameObject.name == gameObject.name + "1")
+        {
+
+            if (col.transform.rotation == gameObject.transform.rotation)
+            {
+                lockObj = col;
+                pieceStatusLock = true;
+            }
+            else
+            {
+                pieceStatusLock = false;
+            }
+
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collisonCheck(collision);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collisonCheck(collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        pieceStatusLock = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (mouseMoving)
+        {
+            Vector2 mousePositon = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePositon);
+            transform.localPosition = objPosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float sinceLastClick = Time.time - lastClickTime;
+            GetComponent<SpriteRenderer>().sortingOrder++;
+            mouseMoving = true;
+            if (sinceLastClick <= doubleClickTime)
+            {
+                transform.Rotate(0f, 0f, 45f);
+            }
+            lastClickTime = Time.time;
+
+        }
+
+    }
+    private void OnMouseUp()
+    {
+        mouseMoving = false;
+        GetComponent<SpriteRenderer>().sortingOrder--;
+        if (pieceStatusLock)
+        {
+            
+            transform.position = lockObj.transform.position;
+            if (gameObject.GetComponent<Collider2D>() != null)
+            {
+                Destroy(gameObject.GetComponent<Collider2D>());
+            }
+            if (lockObj != null)
+            {
+                Destroy(lockObj);
+            }
+            int p;
+            int.TryParse(puan.text,out p);
+            p++;
+            if (p>=TotalPieceNumber)
+            {
+                SceneManager.LoadScene(NextLevelName);
+            }
+            puan.text = p.ToString();
+            Debug.Log(puan.text);
+        }
+
     }
 }
--- Tangram Game/Assets/Scripts/QuadrilateralMove.cs
+++ Tangram Game/Assets/Scripts/QuadrilateralMove.cs
@@ -1,18 +1,120 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class QuadrilateralMove : MonoBehaviour
 {
+    const float doubleClickTime = 0.2f;
+    float lastClickTime = 0.0f;
+    bool pieceStatusLock = false;
+    bool mouseMoving;
+    Collider2D lockObj;
+    public int AngleRotations;
+    public int DegreeOfRotation;
+    public Text puan;
+    public int TotalPieceNumber;
+    public string NextLevelName;
+    private void collisonCheck(Collider2D col)
+    {
+        if (col.gameObject.name == gameObject.name + "1")
+        {
+            int objectRotation = Mathf.RoundToInt(gameObject.transform.localEulerAngles.z);
+            int colliderRotation = Mathf.RoundToInt(col.transform.localEulerAngles.z);
+            for (int i = 0; i < AngleRotations; i++)
+            {
+                if (objectRotation == colliderRotation)
+                {
+                    lockObj = col;
+                    pieceStatusLock = true;
+                    return;
+                }
+                else
+                {
+                    pieceStatusLock = false;
+                }
+                objectRotation += DegreeOfRotation;
+                if (objectRotation > 315)
+                {
+                    objectRotation = objectRotation % DegreeOfRotation;
+                }
+
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collisonCheck(collision);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collisonCheck(collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        pieceStatusLock = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (mouseMoving)
+        {
+            Vector2 mousePositon = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePositon);
+            transform.localPosition = objPosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float sinceLastClick = Time.time - lastClickTime;
+
+            mouseMoving = true;
+            GetComponent<SpriteRenderer>().sortingOrder++;
+            if (sinceLastClick <= doubleClickTime)
+            {
+                transform.Rotate(0f, 0f, 45f);
+            }
+            lastClickTime = Time.time;
+
+        }
+
+    }
+    private void OnMouseUp()
+    {
+        mouseMoving = false;
+        GetComponent<SpriteRenderer>().sortingOrder--;
+        if (pieceStatusLock)
+        {
+            transform.position = lockObj.transform.position;
+            transform.rotation = lockObj.transform.rotation;
+            if (gameObject.GetComponent<Collider2D>() != null)
+            {
+                Destroy(gameObject.GetComponent<Collider2D>());
+            }
+            if (lockObj != null)
+            {
+                Destroy(lockObj);
+            }
+            int p;
+            int.TryParse(puan.text, out p);
+            p++;
+            if (p >= TotalPieceNumber)
+            {
+                SceneManager.LoadScene(NextLevelName);
+            }
+            puan.text = p.ToString();
+        }
+
     }
 }
