32c1678ba02eb1c83b1bd4da19fad859bbbe8725
tanerfiliz98 <tanerfiliz98@gmail.com>
2021-06-01 00:33:23 +0300

    Update Screens
    -Add Main Menu screen
    -Add HowToPlay,Credits screen
    -Screen customizations
    -Add Level Reset
    -Add Project Logo
    -Add Levels Home button

--- Tangram Game/Assets/Scripts/ButtonClick.cs
+++ Tangram Game/Assets/Scripts/ButtonClick.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ButtonClick : MonoBehaviour
 {
     // Start is called before the first frame update
     public void LevelOnClick(string LevelName)
     {
         SceneManager.LoadScene(LevelName);
+    }
+
+    public void LevelReset(GameObject obj)
+    {
+        PlayerPrefs.SetInt("SaveLevelIndex", 1);
+        obj.SetActive(false);
+        obj.SetActive(true);
     }
 }
